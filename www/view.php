e964e828a331d5af8a4c5f4e644ec01976d95dbd
Aniek <aniek.eijpe1@gmail.com>
2020-12-03 18:50:29 +0100

    back

--- www/view.php
+++ www/view.php
@@ -6,12 +6,15 @@ include '../reader.php';
 if (!preg_match('/^[a-zA-Z0-9_\-\.]+\.xml$/i', $_GET['kb']))
 	die('Doe eens niet!');
 
-$file = "helloworld.xml";
+$file = first_found_path(array(
+	'./' . $_GET['kb'],
+	'../knowledgebases/' . $_GET['kb']
+));
 
 if (!$file)
 	die('File not found');
 
-$doc = new DOMDocument()
+$doc = new DOMDocument();
 $doc->load($file, LIBXML_NOCDATA & LIBXML_NOBLANKS);
 
 $template = new Template('templates/view.phtml');
