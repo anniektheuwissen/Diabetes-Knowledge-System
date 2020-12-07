7ec8df63cd12276fcce92a7c690635feb68e1baa
Aniek <aniek.eijpe1@gmail.com>
2020-12-07 15:34:36 +0100

    loading file

--- www/index.php
+++ www/index.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'
 			//break;
 
 		case 'run':
-		  header('Location: webfrontend.php?kb' . rawurlencode($file));
+		  header('Location: webfrontend.php?kb=' . rawurlencode($file));
 			exit;
 	}
 }
