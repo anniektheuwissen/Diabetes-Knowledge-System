8e07c63df7f0e6397e9ac148a14bbbcb1e98f783
Anniek Theuwissen <a.theuwissen@student.rug.nl>
2021-01-11 11:20:17 +0100

    try17

--- www/webfrontend.php
+++ www/webfrontend.php
@@ -69,7 +69,7 @@ class WebFrontend
 			if (isset($_POST['answer']))
 				$state->apply(_decode($_POST['answer']));
 			//nieuw:
-			if (isset($_POST['submit']))
+			if (isset($_POST['answers']))
 				foreach ($_POST['answers'] as $value) {
 					if (isset($value))
 						$state->apply(_decode($value));
